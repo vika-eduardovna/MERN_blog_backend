2c12e0c6512651e7f6cc6601e91ed699e0bcf78b
Vika <viktoria.wout@gmail.com>
2023-10-01 12:45:07 +0200

    authorisation checks added

--- index.js
+++ index.js
@@ -21,41 +21,82 @@ app.get('/', (req, res) => {
     res.send('Hello bka bla bla')
 });
 
-/*app.post('/auth/login', (req, res) => {
-    const token = Jwt.sign({
-        email: req.body.email,
-        fullName: 'Viktoria Menting'
-    },
-        'secret123',
-    );
+app.post('/auth/login', async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' })
+        };
 
+        const isValidPassword = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+        if (!isValidPassword) {
+            return res.status(400).json({ message: 'Password or login are not correct' })
+        };
 
-    res.json({
-        success: true,
-        token
-    });
+        const token = Jwt.sign({
+            _id: user._id,
+        },
+            'secret123',
+            {
+                expiresIn: '30d',
+            }
+        );
 
+        const { passwordHash, ...userData } = user._doc
 
-})*/
+        res.json({
+            ...userData,
+            token
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Authorisation is invalid'
+        })
+    }
+})
 
 app.post('/auth/register', registerValidation, async (req, res) => {
+    // checkin if no errors
     try {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json(errors.array());
         };
+
+        //hash encryption
         const password = req.body.password;
         const salt = await bcrypt.genSalt(10);
-        const passwordHash = await bcrypt.hash(password, salt);
+        const hash = await bcrypt.hash(password, salt);
 
+        // creating document in BD
         const doc = new UserModel({
             email: req.body.email,
-            passwordHash,
+            passwordHash: hash,
             fullName: req.body.fullName,
             avatarUrl: req.body.avatarUrl,
         });
+
+        //document was created
         const user = await doc.save();
-        res.json(user)
+
+        //encrypt _id only
+        const token = Jwt.sign({
+            _id: user._id,
+        },
+            'secret123',
+            {
+                expiresIn: '30d',
+            }
+        );
+
+        const { passwordHash, ...userData } = user._doc // exclude passwordHash from user._doc
+
+        res.json({
+            ...userData,
+            token
+        });
+
     } catch (error) {
         console.log(error);
         res.status(500).json({
